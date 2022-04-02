78bce9f26d5c322ec5b65f650bd1b784d441c309
Amanda Dell <amhernandez1163@gmail.com>
2022-04-01 23:53:14 -0500

    added questions for soon to be prompt code

--- index.js
+++ index.js
@@ -2,7 +2,38 @@
 const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input", 
+        name: "projectName"
+        message: "What is the title of your project?"
+    }, 
+    {
+        type: "input",
+        name: "description",
+        message: "Please provide a brief description of your project. What is it's purpose and user story?", 
+    },
+    {
+        type: "input",
+        name: "instructions",
+        message: "Best way for users to install your application:",       
+      },
+      {
+        type: "input"
+        name: "usage",
+        message: "Best practice to use and optimize your application:";
+      },
+      {
+        type: "input",
+        name: "contributionGuidelines"
+        message: "How would you like users to contribute to this project?",
+      }
+      {
+        type: "input",
+        name: "testInstructions",
+        message: "How should users test this application?"
+      }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
