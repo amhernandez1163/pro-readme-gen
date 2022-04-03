7ec812c73fcf23fbe610329b50758279d60a55ac
Amanda Dell <amhernandez1163@gmail.com>
2022-04-03 11:25:19 -0500

    added function to call questions array and console log responses

--- index.js
+++ index.js
@@ -1,45 +1,54 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
+const fs = require("fs");
 
 // TODO: Create an array of questions for user input
 const questions = [
-    {
-        type: "input", 
-        name: "projectName"
-        message: "What is the title of your project?"
-    }, 
-    {
-        type: "input",
-        name: "description",
-        message: "Please provide a brief description of your project. What is it's purpose and user story?", 
-    },
-    {
-        type: "input",
-        name: "instructions",
-        message: "Best way for users to install your application:",       
-      },
-      {
-        type: "input"
-        name: "usage",
-        message: "Best practice to use and optimize your application:";
-      },
-      {
-        type: "input",
-        name: "contributionGuidelines"
-        message: "How would you like users to contribute to this project?",
-      }
-      {
-        type: "input",
-        name: "testInstructions",
-        message: "How should users test this application?"
-      }
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message:
+      "Please provide a brief description of your project. What is it's purpose and user story?",
+  },
+  {
+    type: "input",
+    name: "instructions",
+    message: "Best way for users to install your application:",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Best practice to use and optimize your application:",
+  },
+  {
+    type: "input",
+    name: "contributionGuidelines",
+    message: "How would you like users to contribute to this project?",
+  },
+  {
+    type: "input",
+    name: "testInstructions",
+    message: "How should users test this application?",
+  },
+  {
+    type: "list",
+    name: "licenseType",
+    message:
+      "What type of license does this project have? Check all that apply.",
+    choices: ["MIT", "GPL 2.0", "APACHE 2.0", "ISC", "MPL", "BSD 2.0"],
+  },
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+// prompts the user for questions array then console logs data input entered by user
+inquirer.prompt(questions).then((data) => {
+  console.log(data);
+}).then;
